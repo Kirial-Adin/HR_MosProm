580fc89fa7e79f4442466fc91efb086644d9386e
Fetth326 <kruglovegor0906@icloud.com>
2025-10-17 23:30:39 +0300

    create login

--- front-end/app/app.vue
+++ front-end/app/app.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+body {
+  background-color: #1a1a1a;
+}
+
+#__nuxt {
+  min-height: 100vh;
+}
+</style>
