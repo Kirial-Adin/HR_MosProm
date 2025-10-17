380d670dd32fef82a19652ce52db8a0b3295a0cd
Kirill Mineev <kirial.adin@yandex.ru>
2025-10-17 23:47:30 +0300

    Add auth

---
+++ front-end/app/components/auth/vuz/SignupVUZ.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import UiBorder from "~/components/ui/UiBorder.vue";
+</script>
+
+<template>
+  <UiBorder>
+    <div class="auth-container">
+      <h1 class="auth-title">Создание аккаунта</h1>
+    </div>
+  </UiBorder>
+</template>
+
+<style lang="scss" scoped>
+.auth-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ front-end/app/components/ui/UiBorder.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="border"><slot></slot></div>
+</template>
+
+<style lang="scss" scoped>
+.border {
+  margin: 10px;
+  width: 98.5%;
+  height: 97vh;
+  border: 1px #3d3d3d solid;
+  border-radius: 23px;
+}
+</style>
